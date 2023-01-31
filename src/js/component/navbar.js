7ab14027f07f39d882d92282b2d2e9fec1b35f74
xControlFreak <xsimplemail@gmail.com>
2023-01-31 21:44:49 +0000

    Falta: unique Favorites=true y Views

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -30,7 +30,7 @@ export const Navbar = () => {
 				<div className="ml-auto">	
 					<div className="btn-group">
 						<button type="button" className="btn btn-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-							Favourites
+							Favorites
 							
 							{/* <span className="text-danger">
                             {
